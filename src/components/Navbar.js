ba2a04f60c9130685a65f08c831c10983e907bc6
prin-r <lang_zane@hotmail.com>
2019-02-11 15:17:43 +0700

    fix header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ import { NavLink } from 'react-router-dom';
 export default props => (
   <PageContainer>
     <Flex flexDirection="row" textAlign="center" justifyContent="center">
-      <Box width={1/2} style={{ textAlign:"center"}}>
+      <Box width={1/2} style={{textAlign:"center"}}>
         <NavLink to="/faucet" activeClassName="is-active" exact={true} >Faucet </NavLink>
       </Box>
-      <Box width={1/2} style={{ textAlign:"center"}}>
+      <Box width={1/2} style={{textAlign:"center"}}>
         <NavLink to="/createcommunity" activeClassName="is-active" exact={true} >Create Community </NavLink>
       </Box>
     </Flex>
